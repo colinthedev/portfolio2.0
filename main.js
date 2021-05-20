c36c973c3a0bea30286d4df79da7263a9e8d87ff
Colin Peters <colinthedev@gmail.com>
2021-05-19 20:25:21 -0400

    skills section javascript, form section

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Random color for line 
+// Random color for line landing page 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
@@ -135,10 +135,12 @@ nikeBackground.addEventListener('mouseleave', function() {
     projectRowWrapNike.classList.remove('active');
 });
 
-//Change font "My current skillset"
+// --Skills Section--------------------------------------------------------------------
+
+//Skills section change font "My current skillset"
 let newString = document.getElementById('colorFont').innerHTML = 'My current skillset'.replace('My', '<span style="color: #fff;">My</span>');
 
-//Card component
+//Skills section card component
 let cardContainer = document.querySelector('.cardRowWrapper');
 let cards = document.querySelectorAll('.card__inner');
 
@@ -155,18 +157,188 @@ cards.forEach(el => {
     });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
+//Skills section background animation
+let withinViewport = (function() {
+    if (window.requestAnimationFrame) {  
+      // Throttle
+      let throttle = function(func, wait, options) {
+        let _ = {
+          now: Date.now || function() {
+            return new Date().getTime();
+          }
+        };
+        let context, args, result;
+        let timeout = null;
+        let previous = 0;
+        if (!options) {
+          options = {};
+        }
+        let later = function() {
+          previous = options.leading === false ? 0 : _.now();
+          timeout = null;
+          result = func.apply(context, args);
+          if (!timeout) {
+            context = args = null;
+          }
+        };
+        return function() {
+          let now = _.now();
+          if (!previous && options.leading === false) {
+            previous = now;
+          }
+          let remaining = wait - (now - previous);
+          context = this;
+          args = arguments;
+          if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+              clearTimeout(timeout);
+              timeout = null;
+            }
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) {
+              context = args = null;
+            }
+          } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+          }
+          return result;
+        };
+      };
+      
+      // requestAnimationFrame
+      let _requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame;
+  
+      // Global class for revealing element
+      let revealer = document.querySelectorAll('.cardContainer');
+      let hidden = document.querySelector('.hidden');
+  
+      // Get the viewport (window) dimensions
+      let getViewportSize = function() {
+        return {
+          width: window.document.documentElement.clientWidth,
+          height: window.document.documentElement.clientHeight
+        };
+      };
+  
+      // Get the current scoll position
+      let getCurrentScroll = function() {
+        return {
+          x: window.pageXOffset,
+          y: window.pageYOffset
+        };
+      };
+  
+      // Get element dimensions and position
+      let getElemInfo = function(elem) {
+        let offsetTop = 0;
+        let offsetLeft = 0;
+        let offsetHeight = elem.offsetHeight;
+        let offsetWidth = elem.offsetWidth;
+  
+        do {
+          if (!isNaN(elem.offsetTop)) {
+            offsetTop += elem.offsetTop;
+          }
+          if (!isNaN(elem.offsetLeft)) {
+            offsetLeft += elem.offsetLeft;
+          }
+        } while ((elem = elem.offsetParent) !== null);
+  
+        return {
+          top: offsetTop,
+          left: offsetLeft,
+          height: offsetHeight,
+          width: offsetWidth
+        };
+      };
+  
+      // Check visibility of the element in the viewport
+      let checkVisibility = function(elem) {
+        let viewportSize = getViewportSize();
+        let currentScroll = getCurrentScroll();
+        let elemInfo = getElemInfo(elem);
+        let spaceOffset = 0.2;
+        let elemHeight = elemInfo.height;
+        let elemWidth = elemInfo.width;
+        let elemTop = elemInfo.top;
+        let elemLeft = elemInfo.left;
+        let elemBottom = elemTop + elemHeight;
+        let elemRight = elemLeft + elemWidth;
+  
+        let checkBoundaries = function() {
+          // Defining the element boundaries and extra space offset
+          let top = elemTop + elemHeight * spaceOffset;
+          let left = elemLeft + elemWidth * spaceOffset;
+          let bottom = elemBottom - elemHeight * spaceOffset;
+          let right = elemRight - elemWidth * spaceOffset;
+  
+          // Defining the window boundaries and window offset
+          let wTop = currentScroll.y + 0;
+          let wLeft = currentScroll.x + 0;
+          let wBottom = currentScroll.y - 0 + viewportSize.height;
+          let wRight = currentScroll.x - 0 + viewportSize.width;
+  
+          // Check if the element is within boundary
+          return (top < wBottom) && (bottom > wTop) && (left > wLeft) && (right < wRight);
+        };
+  
+        return checkBoundaries();
+      };
+  
+      // Run a loop with checkVisibility() and add / remove classes to the elements
+      let toggleElement = function() {
+        for (let i = 0; i < revealer.length; i++) {
+          if (checkVisibility(revealer[i])) {
+            revealer[i].classList.add('revealed');
+            hidden.classList.remove('hidden');
+          } else {
+            revealer[i].classList.remove('revealed');
+            hidden.classList.add('hidden');
+          }
+        }
+      };
+  
+      // Throttle events and requestAnimationFrame
+      let scrollHandler = throttle(function() {
+        _requestAnimationFrame(toggleElement);
+      }, 300);
+  
+      let resizeHandler = throttle(function() {
+        _requestAnimationFrame(toggleElement);
+  
+        // For demo purposes only
+        fullscreenIntro();
+      }, 300);
+  
+      scrollHandler();
+  
+      // Listening for events
+      if (window.addEventListener) {
+        addEventListener('scroll', scrollHandler, false);
+        addEventListener('resize', resizeHandler, false);
+      } else if (window.attachEvent) {
+        window.attachEvent('onscroll', scrollHandler);
+        window.attachEvent('onresize', resizeHandler);
+      } else {
+        window.onscroll = scrollHandler;
+        window.onresize = resizeHandler;
+      }
+    }
+  
+    // FOR DEMO PURPOSES ONLY
+    // Makes fullscreen intro on any device so user is forced to scroll
+    let fullscreenIntro = function() {
+        let fullscreen = document.querySelectorAll('.fullscreen');
+      for (let i = 0; i < fullscreen.length; i++) {
+        fullscreen[i].style.height = getViewportSize().height + 1 + 'px';
+      }
+    };
+    fullscreenIntro();
+  
+    return withinViewport;
+  
+}());
 
 
 
