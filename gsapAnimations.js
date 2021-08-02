aecdd5c38903ba07e69e26169eb9ef77bdf24e3b
Colin Peters <colinthedev@gmail.com>
2021-08-02 15:12:57 -0600

    animations refactored javascript to seperate folders

---
+++ gsapAnimations.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// GSAP Animations
+    gsap.registerPlugin(ScrollTrigger);
+
+// Prevent useless warnings in console if class is not found
+    gsap.config({
+        nullTargetWarn: false,
+    });
+
+// Bouncing green monster
+    let greenMonstTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.contentWrapperRight',
+            start: 'center bottom'
+        },
+    })
+
+    greenMonstTl
+        .from('.bounceAway', {opacity: 0, delay: 3.5, duration: 3})
+
+// Stars svg
+    let hideStarsDefaults = {
+        opacity: 0,
+        duration: 2,
+    }
+
+    let showStarsDefaults = {
+        opacity: 1,
+        duration: 2,
+    }
+
+    let starsTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: '.rightWrapper',
+            start: 'center bottom'
+        },
+    })
+
+    starsTl
+        .from('.star1', {...hideStarsDefaults}, '-=1.5')
+        .from('.star2', {...hideStarsDefaults})
+        .from('.star3', {...hideStarsDefaults}, '-=1.5')
+        .from('.star5', {...hideStarsDefaults})
+        .from('.star1', {...showStarsDefaults}, '-=1.5')
+        .from('.star2', {...showStarsDefaults})
+        .from('.star3', {...showStarsDefaults}, '-=1.5')
+        .from('.star5', {...showStarsDefaults})
+        .to('.star4', {duration: 3, opacity: 1, x: -2000, y: -1000,}, '+=.5')
+        .from('.star', {opacity: 1, x: 0, y: 0}, '+=.5')
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // Fix for nav not working on desktops -> firefox 
     if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
         // Smooth scrolling
@@ -181,122 +182,6 @@ for (let i = 0; i < slide.length; i++ ) {
         });
     });
 
-//  Scroll Magic Controller's      
-    let controller = new ScrollMagic.Controller(); // controller for desktop scenes
-    let controllerMobile = new ScrollMagic.Controller();// controller for mobile scenes
-
-// Skills section scenes --------------------------------------------------
-    // Swipe animation    
-    let sceneBackground = new ScrollMagic.Scene({
-        triggerElement: '.cardContainer',
-        offset: -200, // start this scene -180px from top of container
-        duration: 825, // the scene last for a scroll distance of 800px
-        reverse: false
-    })
-    .setClassToggle('.cardContainer', 'revealed')
-    // sceneBackground.addIndicators()
-    .addTo(controller);
-
-    // Opacity
-    let sceneFade = new ScrollMagic.Scene({
-        triggerElement: '.skillsContainer',
-        offset: -200, // start this scene -180px from top of container
-        duration: 950, // the scene last for a scroll distance of 800px
-        reverse: true
-    })
-    .setClassToggle('.skillsContainer', 'show')
-    // sceneFade.addIndicators()
-    .addTo(controller);
-
-
-// Lightbulb section scenes --------------------------------------------------
-    // Fade
-    let sceneFadeBulb = new ScrollMagic.Scene({
-        triggerElement: '.bulbWrapper',
-        offset: 750,
-        reverse: false
-    })
-    .setClassToggle('.bulbWrapper', 'show-bulb')
-    .addTo(controller);
-    // sceneFadeBulb.addIndicators()
-
-    // Swing
-    let sceneBulb =  new ScrollMagic.Scene({
-        triggerElement: '.bulbWrapper',
-        offset: 750,
-        reverse: false
-    })
-    .setClassToggle('.bulbWrapper', 'swing')
-    .addTo(controller);
-    // sceneBulb.addIndicators()
-
-
-// About section scenes -----------------------------------------------------
-    // Text slide left
-    let revealElements = document.getElementsByClassName("slide-left");
-    for (let i = 0; i < revealElements.length; i++) { // create a scene for each element
-        new ScrollMagic.Scene({
-                            triggerElement: revealElements[i],
-                            offset: -11,
-                            reverse: false,
-                            triggerHook: 0.9, // start a little later
-                        })
-                        .setClassToggle(revealElements[i], "visible") // add class toggle
-                        .addTo(controller);
-                        // .addIndicators({name: "digit " + (i+1) })
-    }
-
-    // Content fade in
-    new ScrollMagic.Scene({
-        triggerElement: "#trigger1",
-        offset: -150, 
-        reverse: false,
-        triggerHook: 0.7 // start a little later
-    })
-    .setClassToggle("#reveal1", "visible") // add class to reveal
-    .addTo(controller);
-    // .addIndicators()
-
-
-// Contact section scenes ----------------------------------------------------
-    // Text slide right
-    let sceneContact = new ScrollMagic.Scene({
-        triggerElement: '.slide-right',
-        offset: -370, // start this scene -180px from top of container
-        reverse: false
-    })
-    .setClassToggle('.slide-right', 'textReveal')
-    .addTo(controller);
-    // sceneContact.addIndicators()
-
-    // Pointing animation 
-    let sceneArrow = new ScrollMagic.Scene({
-        triggerElement: '.contactArrow',
-        offset: -80, // start this scene -180px from top of container
-        duration: 1100, // the scene last for a scroll distance of 800px
-        reverse: true
-    })
-    .setClassToggle('.contactArrow', 'showArrow')
-    .addTo(controller);
-    // sceneArrow.addIndicators()
-
-
-// Projects mobile scenes --------------------------------------------------------
-    // Fade in
-    let revealProjects = document.getElementsByClassName("projectsWrapperMobile");
-    for (let i = 0; i < revealProjects.length; i++) { // create a scene for each element
-        new ScrollMagic.Scene({
-                            triggerElement: revealProjects[i],
-                            offset: 200,
-                            reverse: false,
-                            triggerHook: 0.9, // start a little later
-                        })
-                        .setClassToggle(revealProjects[i], 'visible') // add class toggle
-                        .addTo(controllerMobile);
-                        // .addIndicators({name: "digit " + (i+1) })
-    }
-
-
 // Chevron back to top
     let toTop = document.querySelector('.chevronUp');
 
@@ -304,60 +189,6 @@ for (let i = 0; i < slide.length; i++ ) {
         document.body.scrollTop = 0; // For Safari
         document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     });
-
-// Media querys --------------------------------------------------------------
-    // Skills section
-        function skillsAnimation() {
-            let skillsSection = document.querySelector('.skillsContainer');
-            
-            skillsSection.style.opacity = '1';
-            skillsSection.style.transition = 'none';
-            skillsSection.style.webkittransition = 'none';
-        };
-    // About section 
-        function aboutAnimation() {
-            for (let i = 0; i < slide.length; i++ ) { // Slide text
-                let aboutSectionTop = document.querySelectorAll('.slide-left');
-
-                aboutSectionTop[i].style.transform = 'none';
-                aboutSectionTop[i].style.webkittransform = 'none';
-                aboutSectionTop[i].style.transition = 'none';
-                aboutSectionTop[i].style.webkittransition = 'none';
-                aboutSectionTop[i].style.opacity = '1';
-            };
-
-            let aboutSectionBot = document.getElementById('reveal1'); // Fade text
-
-            aboutSectionBot.style.transform = 'none';
-            aboutSectionBot.style.webkittransform = 'none';
-            aboutSectionBot.style.transition = 'none';
-            aboutSectionBot.style.webkittransition = 'none';
-            aboutSectionBot.style.opacity = '1';
-        };
-    // Contact section
-        function contactAnimation() {
-            let contactSection = document.querySelector('.slide-right');
-            
-            contactSection.style.transform = 'none'; // Slide text
-        };
-
-    // Media query function
-        let x = window.matchMedia("(max-width: 856px)");
-    // Disable enable ScrollMagic controller
-        function myFunction(x) {
-            if (x.matches) { // If media query matches
-            controller.enabled(false); // ScrollMagic
-            skillsAnimation() // Skills section
-            aboutAnimation(); // About section 
-            contactAnimation(); // Contact section
-            } else {
-            controller.enabled(true);
-            }
-            controller.update(true);
-        };
-        
-        myFunction(x); // Call listener function at run time
-        //   x.addListener(myFunction) // Attach listener function on state changes 
 
 
 
